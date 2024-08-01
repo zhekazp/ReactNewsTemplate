659d5142896531e19dea27f1cdcc2fa7b0fb10c3
17101965 <vzvagolsky@gmail.com>
2024-08-01 07:51:12 +0200

    Viktor 0108 7:50

--- src/config/menuConfig.ts
+++ src/config/menuConfig.ts
@@ -1,8 +1,11 @@
 export const menuItems = [
     { title: 'Home', path: '/' },
     { title: 'Nachrichten', path: '/news' },
-    { title: 'Blog', path: '/blogs' },
+    { title: 'Blog', path: '/blog' },
     { title: 'Anzeige', path: '/advertisement' },
     { title: 'Über Uns', path: '/about' },
+	{ title: 'Gewinnen', path: '/api/ads' },
+	{ title: 'Gewinnen', path: '/gewinnen' },
     { title: 'Kontakt', path: '/contact' },
+	
   ];
