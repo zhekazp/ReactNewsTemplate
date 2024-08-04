e9ab74b3a76a14cb58f9d43e95e2ebe48b52f85c
MaxOpenkin <openkin.max@gmail.com>
2024-08-04 19:15:51 +0200

    update navigation

--- src/features/blog/blogs/Breadcrumb.tsx
+++ src/features/blog/blogs/Breadcrumb.tsx
@@ -6,30 +6,6 @@ const Breadcrumb: React.FC = () => {
   const location = useLocation();
   const pathnames = location.pathname.split('/').filter(x => x);
 
-//   return (
-//     <nav aria-label="breadcrumb">
-//       <ol className="breadcrumb">
-//         <li className="breadcrumb-item">
-//           <Link to="/">Home</Link>
-//         </li>
-//         {pathnames.map((value, index) => {
-//           const to = `/${pathnames.slice(0, index + 1).join('/')}`;
-
-//           return index === pathnames.length - 1 ? (
-//             <li key={to} className="breadcrumb-item active" aria-current="page">
-//               {value}
-//             </li>
-//           ) : (
-//             <li key={to} className="breadcrumb-item">
-//               <Link to={to}>{value}</Link>
-//             </li>
-//           );
-//         })}
-//       </ol>
-//     </nav>
-//   );
-// };
-
 
 return (
   <nav aria-label="breadcrumb">
