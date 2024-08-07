2470d672eae8388d4680ffc884f5887d75347e12
zhekazp <zhekazp21@gmail.com>
2024-08-07 12:38:27 +0300

    about

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <>
+    <div className="container mainBlock textColorMain textCenter">
+      <h2>Bundesland Blog</h2>
+      <p>Diese Website dient Bildungszwecken. Die Seite wurde von Studierenden der AIT-Schule (IT-Studiengänge in Deutschland) verfasst.</p> 
+      <p>Dies ist ein lebendiges Beispiel dafür, was Sie in einem 7-monatigen Training lernen können.</p> 
+      <p>Die Seite wurde in 2 Wochen von Grund auf neu geschrieben. Ein Team von 9 Leuten.</p>
+    </div>
+    </>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
