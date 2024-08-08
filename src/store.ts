6e91b2a0517a6bccc7f79bb87761d38d80937edb
zhekazp <zhekazp21@gmail.com>
2024-08-08 13:57:00 +0300

    store

--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import newsReducer from "./features/news/newsSlice";
 import contactUsReducer from "./features/contactUs/contactUsSlice";
-
-// import blogReducerTest from "./features/blog/blogsTest/blogSliceTest";
 import blogReducer from "./features/blog/blogs/blogSlice";
 import  topSliceReducer  from "./layout/header/topElSlice";
 
-
-
-// Заглушка редьюсера
-const dummyReducer = (state = {}, action: any) => state;
-
 const store = configureStore({
   reducer: {
-    dummy: dummyReducer,
     news: newsReducer,
-       contactForm: contactUsReducer,
-    // blogSliceTest: blogReducerTest,
+    contactForm: contactUsReducer,
     blogs: blogReducer,
     top: topSliceReducer
   },
