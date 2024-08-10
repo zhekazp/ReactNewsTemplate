e564bfe60ff4f84bb8a50f4209e7cd89b369fdc5
zhekazp <64210219+zhekazp@users.noreply.github.com>
2024-08-10 10:23:33 +0300

    Update NewsDetail.tsx

--- src/pages/NewsDetail.tsx
+++ src/pages/NewsDetail.tsx
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-import React, { FC, useEffect } from 'react'
-import { AppDispatch, RootState } from '../store';
-import { useDispatch, useSelector} from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
-import { fetchNewsById } from '../features/news/newsSlice';
 
-const NewsDetail: FC = () => {
-    const dispatch: AppDispatch = useDispatch();
-    
-=======
 import React, { FC, useEffect, useState } from 'react'
 import { AppDispatch, RootState } from '../store';
 import { useDispatch, useSelector } from 'react-redux';
@@ -23,69 +13,13 @@ import { topSlice } from '../layout/header/topElSlice';
 const NewsDetail: FC = () => {
     const dispatch: AppDispatch = useDispatch();
 
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
+
     const { id } = useParams();
 
     const newsItem = useSelector((state: RootState) => state.news.selectedNews);
     const status = useSelector((state: RootState) => state.news.status);
 
-<<<<<<< HEAD
-    useEffect(() => {
-        if (id) {
-            dispatch(fetchNewsById(Number(id)));
-        }
-    }, [dispatch, id]);
 
-    useEffect(() => {
-        console.log("NewsDetail status:", status);
-        console.log("Selected news item:", newsItem);
-      }, [status, newsItem]);
-
-    const handleLike = () => {
-        // if (newsItem.isUserLikes) {
-        //     dispatch(unlikeNews(newsItem.newsId));
-        // } else {
-        //     dispatch(likeNews(newsItem.newsId));
-        // }
-    };
-
-    const handleDislike = () => {
-        // if (newsItem.isUserDislikes) {
-        //     dispatch(undislikeNews(newsItem.newsId));
-        // } else {
-        //     dispatch(dislikeNews(newsItem.newsId));
-        // }
-    };
-    
-  return (
-    <div>
-         {status === "loading" && (
-                <div className="spinner-border text-warning" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
-            )}
-            { status === 'success' && newsItem &&(
-              <div>
-                 <h1>{newsItem.title}</h1>
-            <img src={newsItem.titleImageWide} alt={newsItem.title} />
-            <p>{newsItem.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: newsItem.content }}></div>
-            <button onClick={handleLike}>
-                {newsItem.isUserLikes ? 'Unlike' : 'Like'} ({newsItem.likes})
-            </button>
-            <button onClick={handleDislike}>
-                {newsItem.isUserDislikes ? 'Remove Dislike' : 'Dislike'} ({newsItem.dislikes})
-            </button>
-              </div>
-              
-            )}
-            {status === "error" && (
-                <>Error load newsItem!</>
-            )}
-       
-    </div>
-  )
-=======
     const comments = useSelector((state: RootState) => state.news.comments);
 
     const [comment, setComment] = useState('');
@@ -298,7 +232,7 @@ const NewsDetail: FC = () => {
 
         </section>
     )
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
+
 }
 
 export default NewsDetail
