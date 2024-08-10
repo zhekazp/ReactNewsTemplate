12d39ae399f6d85ea45ddeedd02b6e461c817bc5
zhekazp <64210219+zhekazp@users.noreply.github.com>
2024-08-10 10:19:35 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,32 +10,23 @@ import { uid } from "uid";
 import Blog from "../features/mainPage/components/blog/Blog";
 import InfoComponent from "../features/mainPage/components/info/InfoComponent";
 import { MainPageData, NewsMP, WeatherMP } from "../features/mainPage/mainPage";
-<<<<<<< HEAD
-import ErrorModal from "../features/mainPage/components/modal/ErrorModal";
 
-
-=======
 // import ErrorModal from "../features/mainPage/components/modal/ErrorModal";
 import { useDispatch } from "react-redux";
 import { topSlice } from "../layout/header/topElSlice";
 import type { INewsTop } from "../layout/header/topElSlice";
 import { Link } from "react-router-dom";
 import ModalComponent from "../features/mainPage/components/modal/ModalComponent";
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
+
 const Home = () => {
   const [data, setData] = useState<MainPageData | null>(null);
   const [errorResponse, setError] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
   const [weather, setWeather] = useState<WeatherMP | null>(null);
   const [weatherLoading, setStatus] = useState<boolean>(true);
-<<<<<<< HEAD
-
-  useEffect(() => {
-=======
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(topSlice.actions.setCurrentPage(0));
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
     fetch("/api/mainpage")
       .then((response) => {
         if (response.status > 299) {
@@ -44,28 +35,6 @@ const Home = () => {
         return response;
       })
       .then((response) => response.json())
-<<<<<<< HEAD
-      .then((data) => setData(data))
-      .catch(()=>{
-        setLoading(false);  
-        setError(true);
-      }
-      );
-     
-    fetch("/api/mainpage/weather")
-    .then((response) => {
-      if (response.status > 299) {
-        throw new Error("Server Error");
-      }
-      setStatus(false);
-      return response;
-    })
-    .then((res) => res.json())
-    .then((data) => setWeather(data))
-    .catch(()=>
-        setStatus(false)
-    );
-=======
       .then((data) => {
         setData(data);
         const newsDT: INewsTop[] = data.innerNews.slice(12, 15).map((item) => {
@@ -89,20 +58,13 @@ const Home = () => {
       .then((res) => res.json())
       .then((data) => setWeather(data))
       .catch(() => setStatus(false));
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
+
   }, []);
 
   return (
     <>
       {data === null ? (
         <>
-<<<<<<< HEAD
-        <Spinner show={loading} color="red" />
-        <ErrorModal show={errorResponse} onHide={()=>{setError(false); console.log(ErrorModal);
-        }} />
-        </>
-        ) : (
-=======
           <Spinner show={loading} color="red" />
           <ModalComponent  show={errorResponse}
              title="Error"
@@ -113,7 +75,6 @@ const Home = () => {
              />
         </>
       ) : (
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
         <>
           <div className=".bg-dark"></div>
           <div className="container mainBlock">
@@ -122,19 +83,6 @@ const Home = () => {
                 <Carousel fade={true}>
                   {data.innerNews.slice(0, 4).map((newsItem: NewsMP) => (
                     <Carousel.Item key={uid()}>
-<<<<<<< HEAD
-                      <div className="picBlock">
-                        <img
-                          width="100%"
-                          src={newsItem.titleImageSquare}
-                          alt={newsItem.title}
-                        />
-                      </div>
-                      <Carousel.Caption>
-                        <h4>{newsItem.title}</h4>
-                        <NewsInfo info={newsItem} />
-                      </Carousel.Caption>
-=======
                       <Link to={"/news/" + newsItem.id}>
                         <div className="picBlock">
                           <img
@@ -149,7 +97,6 @@ const Home = () => {
                           <NewsInfo info={newsItem} />
                         </Carousel.Caption>
                       </Link>
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
                     </Carousel.Item>
                   ))}
                 </Carousel>
@@ -158,13 +105,9 @@ const Home = () => {
                 <div className="row ">
                   {data.innerNews.slice(5, 7).map((newsItem) => (
                     <div key={uid()} className="col-6">
-<<<<<<< HEAD
-                      <News city={false} info={newsItem} />
-=======
                       <Link to={"/news/" + newsItem.id}>
                         <News city={false} info={newsItem} />
                       </Link>
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
                     </div>
                   ))}
                 </div>
@@ -177,14 +120,6 @@ const Home = () => {
             </div>
             <Ad text="Bestes Web-Erstellungsteam – klicken Sie hier, um einen Rabatt zu erhalten" />
             <div className="row">
-<<<<<<< HEAD
-              <div className="col-12">
-                <Title title="Bundesland Nachrichten" />
-                <div className="row">
-                  {data.innerNews.slice(7, 11).map((item) => (
-                    <div key={uid()} className="col-3">
-                      <News city={true} info={item} />
-=======
               <div className="col-12 smallWidth">
                 <Title title="Bundesland Nachrichten" />
                 <div className="row">
@@ -193,17 +128,12 @@ const Home = () => {
                       <Link to={"/news/" + item.id}>
                         <News city={true} info={item} />
                       </Link>
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
                     </div>
                   ))}
                 </div>
               </div>
 
-<<<<<<< HEAD
-              <div className="col-12 newsTopic2">
-=======
               <div className="col-12 newsTopic2 smallWidth">
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
                 <div className="row">
                   <div className="col-6 halfScreen">
                     <Title title="Neueste Anzeige" />
@@ -216,13 +146,9 @@ const Home = () => {
                     <div className="row">
                       {data.world.map((item) => (
                         <div key={uid()} className="col-6 worldNews">
-<<<<<<< HEAD
-                          <News city={false} info={item} />
-=======
                           <Link to={"/news/" + item.id}>
                             <News city={false} info={item} />
                           </Link>
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
                         </div>
                       ))}
                     </div>
@@ -238,15 +164,6 @@ const Home = () => {
                 <Carousel fade={true}>
                   {[0, 2, 4].map((item) => (
                     <Carousel.Item key={uid()}>
-<<<<<<< HEAD
-                      <div className="row">
-                        {[0, 1].map((childItem) => (
-                          <div key={uid()} className="col-6">
-                            <News
-                              city={false}
-                              info={data.sport[item + childItem]}
-                            />
-=======
                       <div className="row newsTopic">
                         {[0, 1].map((childItem) => (
                           <div key={uid()} className="col-6">
@@ -258,7 +175,6 @@ const Home = () => {
                                 info={data.sport[item + childItem]}
                               />
                             </Link>
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
                           </div>
                         ))}
                       </div>
@@ -269,13 +185,9 @@ const Home = () => {
               <div className="col-6 newSlider">
                 <Title title="Blogs" />
                 {data.blogs.map((item) => (
-<<<<<<< HEAD
-                  <Blog key={uid()} info={item} />
-=======
                   <Link key={uid()} to={"/blogs/"+item.id}>
                     <Blog info={item} />
                   </Link>
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
                 ))}
               </div>
             </div>
