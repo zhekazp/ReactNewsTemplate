c63f4e9cf9e4507bb8e7bceaf1497e5390697c37
zhekazp <64210219+zhekazp@users.noreply.github.com>
2024-08-10 10:21:51 +0300

    Update News.tsx

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -2,81 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, RootState } from '../store';
 import NewsComponent from '../features/news/NewsComponent';
-<<<<<<< HEAD
-import { fetchNews, fetchNewsBySection, INewsItem } from '../features/news/newsSlice';
-import  Dropdown  from 'react-bootstrap/DropDown';
-=======
+
 import { fetchFilteredNews, fetchNews, fetchRegions, fetchSections } from '../features/news/newsSlice';
 import { topSlice } from "../layout/header/topElSlice";
 import Spinner from '../features/mainPage/components/spinner/Spinner';
 import ResponsivePagination from 'react-responsive-pagination';
 import '../style/news.css'
 
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
-
 const News = () => {
   const dispatch: AppDispatch = useDispatch();
 
-<<<<<<< HEAD
-  const { newsArr, status, sections } = useSelector((state: RootState) => state.news);
-  const [selectedSection, setSelectedSection] = useState<string | null>(null);
 
-  useEffect(() => {
-    // dispatch(fetchNews());
-
-    if (selectedSection) {
-      dispatch(fetchNewsBySection(selectedSection));
-  } else{
-    dispatch(fetchNews());
-  }
-
-
-  }, [dispatch, selectedSection]);
-
-
-
-
-  const handleSelect = (eventKey: string | null) => {
-    if (eventKey) {
-      setSelectedSection(eventKey);
-  }
-  };
-
-  return (
-    <section>
-      <div className='container d-flex'>
-        <div className='news-aside'>
-          <Dropdown onSelect={handleSelect}>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-            {selectedSection || "Select Section"}
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-            {sections.map((section) => (
-                                <Dropdown.Item 
-                                    key={section} 
-                                    eventKey={section}
-                                >
-                                    {section}
-                                </Dropdown.Item>
-                            ))}
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-        <div className='news-content'>
-          {status === "loading" && (
-            <div className="spinner-border text-warning" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </div>
-          )}
-          {status === 'success' && (newsArr.map((newsItem) => (
-            <NewsComponent
-              key={newsItem.id}
-              newsItem={newsItem}
-            />)
-          ))}
-          {status === "error" && (
-=======
   const { newsArr, status, pageCount, currentPage, sections, regions } = useSelector((state: RootState) => state.news);
 
   const [selectedSection, setSelectedSection] = useState<string | undefined>(undefined);
@@ -182,7 +118,7 @@ const News = () => {
              
             </div>
           ) : (
->>>>>>> 161292b5d4fbb932f5678b4924c3aa9712feb9db
+
             <>Error!</>
           )}
         </div>
