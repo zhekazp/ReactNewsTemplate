cbcdd2fb780bd7895fbf45bb80671c190f0b69f2
AnnaChristy-13 <annachristy07@google.com>
2024-08-11 17:01:05 +0200

    anzege path2

--- src/config/menuPath.ts
+++ src/config/menuPath.ts
@@ -2,7 +2,7 @@ export const menuPath = [
     { index: 0, path: '' },
     { index: 1, path: 'news' },
     { index: 2, path: 'blogs' },
-    { index: 3, path: 'advertisement' },
+    { index: 3, path: 'anzeige' },
     { index: 4, path: 'about' },
 	{ index: 5, path:'ads' },
     { index: 6, path: 'contact' },
