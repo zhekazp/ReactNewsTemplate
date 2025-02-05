02ba847735a21e7d63501f79cb3219e7019fd398
zhekazp <64210219+zhekazp@users.noreply.github.com>
2025-02-05 12:44:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     open: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://blb-app-rzsrl.ondigitalocean.app",
+        changeOrigin: true,
       },
     },
   },
